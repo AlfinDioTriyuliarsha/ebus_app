2cae59841a0936b4f0f1e37af3baf69b6e2ac69a
AlfinDioTriyuliarsha <156634360+AlfinDioTriyuliarsha@users.noreply.github.com>
2026-04-11 12:12:23 +0700

    update : manajemen rute

--- lib/screens/admin_perusahaan/ManajemenRutePage.dart
+++ lib/screens/admin_perusahaan/ManajemenRutePage.dart
@@ -7,7 +7,7 @@ class ManajemenRutePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Manajemen Rute & Zona")),
+      appBar: AppBar(title: const Text("Manajemen Rute")),
       body: const Center(child: Text("Rute Keberangkatan")),
     );
   }
