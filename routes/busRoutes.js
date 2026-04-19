d551e47618319303fa57a09a77273881bf5c8857
AlfinDioTriyuliarsha <156634360+AlfinDioTriyuliarsha@users.noreply.github.com>
2026-04-19 20:17:16 +0700

    update : bus routes

--- routes/busRoutes.js
+++ routes/busRoutes.js
@@ -29,7 +29,7 @@ router.get("/", async (req, res) => {
                         JOIN locations l ON rc.location_id = l.id
                         WHERE rc.route_id = r.id
                     ),
-                    '[]'
+                    '[]'::json
                 ) as route
 
             FROM buses b
