f585e5f67135536281123d227d208ae9fc0ebdcb
AlfinDioTriyuliarsha <156634360+AlfinDioTriyuliarsha@users.noreply.github.com>
2026-06-06 23:18:40 +0700

    disable old service worker

--- routes/firebase.js
+++ routes/firebase.js
@@ -1,12 +1,17 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT
-);
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT
+  );
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
 
-console.log("FIREBASE READY");
+  console.log("🔥 FIREBASE READY");
+} else {
+  console.log("⚠️ FIREBASE_SERVICE_ACCOUNT TIDAK DITEMUKAN");
+}
+
 module.exports = admin;
