5453304267e2b557b834f95a11111658bbe4d4d4
AlfinDioTriyuliarsha <156634360+AlfinDioTriyuliarsha@users.noreply.github.com>
2026-06-06 15:39:40 +0700

    Fix firebase import path

--- routes/busRoutes.js
+++ routes/busRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../db");
 const { broadcastLocation } = require("../websocket");
-const admin = require("../firebase");
+const admin = require('./firebase');
 const geolib = require("geolib");
 
 // =======================
